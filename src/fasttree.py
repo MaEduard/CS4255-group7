5bcce7188725872b021fa70e85b6e44accb9a1c9
Olof Jonnerby <olof.jonnerby@hotmail.com>
2022-12-08 15:26:34 +0100

    finding minimum distance works. The main loop for the program still needs to be implemented

--- src/fasttree.py
+++ src/fasttree.py
@@ -1,5 +1,6 @@
 from sys import stdin
 import math
+from node import Node
 
 
 def read_file(file_name):
@@ -10,7 +11,6 @@ def read_file(file_name):
             sequences.append(line[:len(line)-1])  # to remove the \n
 
     return sequences
-
 
 def letter_to_index(let):
     if let == "A":
@@ -43,6 +43,9 @@ def make_profile(seq):
 
 
 def make_empty_profile(size):
+    """
+    Creates an empty profile filled with zeros. 
+    """
     profile = [[], [], [], []]
     for row in profile:
         for i in range(size):
@@ -52,60 +55,92 @@ def make_empty_profile(size):
 
 
 def profile_join(a, b):
-    # Size of first row from a (sequence length)
+    """
+    Joins two profile together and returns the result. 
+    """
     new_prof = make_empty_profile(len(a[0]))
     for col in range(len(a)):  # Assume same size for a and b
         for row in range(4):  # Only 4 nucleotides.
             new_prof[row][col] = (a[row][col] + b[row][col])/2
-
+    return new_prof
 
 def prof_dist(a, b):
     """
     Calculates and returns the distance between two profiles.
     """
     dist = 0
-    for i in range(len(a)):
+    for i in range(len(a[0])):
         for x in range(4):  # Only 4 nucleotides.
             for y in range(4):
                 if x != y:
                     dist += a[x][i] * b[y][i]
 
-    return dist/len(a)
+    return dist/len(a[0])
 
-def find_min_dist(profile, profiles):
-    dist = 0
+def find_min_dist_nodes(nodes):
+    """
+    Finds the minimum distance between all possible joins, and returns the index of both nodes. 
+
+    Using exhaustive search. 
+    """
     min_dist = float('inf')
-    for prof in profiles:
-        dist = prof_dist(profile, prof)
-        if dist == 0:
-            continue 
-        if dist < min_dist:
-            min_dist = dist 
-            
+    for i in range(len(nodes)):
+        if not nodes[i].is_active: # Skip nodes that are inactive. 
+            continue
+        for j in range(i+1, len(nodes)):
+            if not nodes[j].is_active: # Skip nodes that are inactive. 
+                continue
+            dist = prof_dist(nodes[i].profile, nodes[j].profile) - nodes[i].up_distance - nodes[j].up_distance
+            if dist < min_dist:
+                min_dist = dist
+                ind1 = i
+                ind2 = j
+    return ind1, ind2, min_dist
 
+def initialize_leaf_nodes(seqs) -> list:
+    """ 
+    Creates the initial profiles for the input sequences. 
+    """
+    nodes = []
+    for seq in seqs:
+        profile = make_profile(seq)
+        node = Node(profile, up_distance=0, is_active=True)
+        nodes.append(node)    
+    return nodes
 
-seqs = []
-for line in stdin:
-    line = line.strip()
-    seqs.append(line)
+def print_profile(a):
+    for row in a:
+        print(row)
 
-profiles = []
-for seq in seqs:
-    profile = make_profile(seq)
-    profiles.append(profile)
+def find_joins(nodes:list, active_nodes):
+    """
+    Loops through all active nodes and joins the nodes with the minimum distance. 
+    Stops when the number of active nodes is just one. 
 
-print(profiles[0])
+    TODO Maybe removing the nodes from the list can be used instead of using a bool flag?
+    TODO Node.is_active is currently not working
+    """
+    while active_nodes > 1:
+        a, b, mindist = find_min_dist_nodes(nodes)
+        nodes[a].is_active = False 
+        nodes[b].is_active = False
+        prof_a = nodes[a].profile
+        prof_b = nodes[b].profile
+        c = profile_join(prof_a,prof_b)
+        up_dist = (prof_dist(prof_a,c) + prof_dist(prof_b,c))/2
+        nodes.append(Node(profile=c, up_distance=up_dist, is_active=True))
 
+        active_nodes -=1
 
-class Node:
-    profile = []
-    up_distance = 0
-    is_active = True
-    children = []
+seqs = read_file('../data/test-small.aln')
+nodes = initialize_leaf_nodes(seqs)
+active_nodes = len(nodes)
+print(nodes[0].is_active)
 
+find_joins(nodes, active_nodes)
 
 def main():
-    read_file('data/test-small.aln')
+    read_file('../data/test-small.aln')
 
 
 if __name__ == '__main__':
---
+++ src/node.py
@@ -0,0 +1,16 @@
+class Node:
+    """
+    Class representation of a node in the phylogenetic tree. 
+    """
+    profile: list
+    up_distance: float 
+    is_active: bool
+    children:list
+
+    def __init__(self, profile=[], up_distance=0, is_active=False):
+        self.profile = profile
+        self.up_distance = up_distance
+        is_active = is_active
+    
+    def set_active_status(self, val:bool):
+        self.is_active = val
