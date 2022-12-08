1d9c9bf22fb04976c4b310e00506c8a2625e55d7
MaEduard <eduard.ma6@gmail.com>
2022-12-08 20:44:37 +0100

    Fixes small bug in node class which makes sure node.is_active attribute works

--- src/node.py
+++ src/node.py
@@ -10,7 +10,7 @@ class Node:
     def __init__(self, profile=[], up_distance=0, is_active=False):
         self.profile = profile
         self.up_distance = up_distance
-        is_active = is_active
+        self.is_active = is_active
     
     def set_active_status(self, val:bool):
         self.is_active = val
