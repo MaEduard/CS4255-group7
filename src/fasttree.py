27d2a370e579346c3ef596cdc62bf4c2920450a0
Olof Jonnerby <olof.jonnerby@hotmail.com>
2022-12-10 17:25:34 +0100

    we get the right topology but the branch distances are still not correct

--- src/fasttree.py
+++ src/fasttree.py
@@ -2,6 +2,7 @@ from sys import stdin
 import math
 from node import Node
 
+tot_up_dist = 0
 
 def read_file(file_name):
     f = open(file_name, "r")
@@ -27,11 +28,6 @@ def jukes_cantor(prof):
 
     dist = -(3/4)*math.log(1-(4/3))
 
-
-def total_profile():
-    pass
-
-
 def make_profile(seq):
     """
     Makes a new profile based on one sequence. 
@@ -54,53 +50,78 @@ def make_empty_profile(size):
     return profile
 
 
-def profile_join(a, b):
+def profile_join(a, j):
     """
     Joins two profile together and returns the result. 
     """
     new_prof = make_empty_profile(len(a[0]))
-    for col in range(len(a)):  # Assume same size for a and b
-        for row in range(4):  # Only 4 nucleotides.
-            new_prof[row][col] = (a[row][col] + b[row][col])/2
+    for row in range(4):  # Only 4 nucleotides.
+        for col in range(len(a[0])):  # Assume same size for a and j
+            new_prof[row][col] = (a[row][col] + j[row][col])/2
     return new_prof
 
-def prof_dist(a, b):
+def node_dist(i:Node, j:Node):
     """
-    Calculates and returns the distance between two profiles.
+    Calculates and returns the distance between the profiles of node a and node b
     """
     dist = 0
-    for i in range(len(a[0])):
+    prof_i = i.profile
+    prof_j = j.profile
+    k = len(prof_i[0])
+    for a in range(k):
         for x in range(4):  # Only 4 nucleotides.
             for y in range(4):
                 if x != y:
-                    dist += a[x][i] * b[y][i]
+                    dist += prof_i[x][a] * prof_j[y][a]
 
-    return dist/len(a[0])
+    return dist/k - i.up_distance - j.up_distance
+
+def prof_dist(i, j):
+    """
+    Calculates and returns the distance between the profiles i and j. 
+    """
+    dist = 0
+    k = len(i[0])
+    for a in range(k):
+        for x in range(4):  # Only 4 nucleotides.
+            for y in range(4):
+                if x != y:
+                    dist += i[x][a] * j[y][a]
+
+    return dist/k
 
 def r_equation(node_i, total_profile, n, up_sum):
     ans = n*prof_dist(node_i.profile, total_profile) - prof_dist(node_i, node_i) - (n-1)*node_i.up_distance + node_i.up_distance - up_sum
     return ans/(n-2)
 
-def find_min_dist_nodes(nodes, total_profile):
+def find_min_dist_nodes(nodes, total_profile, n):
     """
     Finds the minimum distance between all possible joins, and returns the index of both nodes. 
 
     Using exhaustive search. 
 
+    Parameters: 
+        n = number of active nodes
+
     TODO include the r_equation to compute r(i) and r(j) to eventually compute d'(i,j) = d(i,j) - r(i) - r(j)
     """
     min_dist = float('inf')
+    ri = 0
+    rj = 0
     for i in range(len(nodes)):
-        if not nodes[i].is_active: # Skip nodes that are inactive. 
-            continue
-        for j in range(i+1, len(nodes)):
-            if not nodes[j].is_active: # Skip nodes that are inactive. 
-                continue
-            dist = prof_dist(nodes[i].profile, nodes[j].profile) - nodes[i].up_distance - nodes[j].up_distance
-            if dist < min_dist:
-                min_dist = dist
-                ind1 = i
-                ind2 = j
+        if nodes[i].is_active: # Skip nodes that are inactive. 
+            for j in range(i+1, len(nodes)):
+                if nodes[j].is_active: # Skip nodes that are inactive. 
+                    dist_i_j = node_dist(nodes[i], nodes[j])
+                    r_i = out_dist(nodes[i], total_profile, n)
+                    r_j = out_dist(nodes[j], total_profile, n)
+                    dist_prime_i_j = dist_i_j - r_i - r_j
+
+                    if dist_prime_i_j < min_dist:
+                        min_dist = dist_prime_i_j
+                        ind1 = i
+                        ind2 = j
+
     return ind1, ind2, min_dist
 
 def initialize_leaf_nodes(seqs) -> list:
@@ -108,10 +129,10 @@ def initialize_leaf_nodes(seqs) -> list:
     Creates the initial profiles for the input sequences. 
     """
     nodes = []
-    for seq in seqs:
+    for index, seq in enumerate(seqs):
         profile = make_profile(seq)
         node = Node(profile=profile, up_distance=0, is_active=True)
-        node.add_sequence(seq)
+        node.indexes = str(index)
         nodes.append(node)    
     return nodes
 
@@ -119,29 +140,34 @@ def print_profile(a):
     for row in a:
         print(row)
 
-def compute_total_profile(seqs):
-    """Computes the profile matrix of a seqs matrix
+def set_total_up_dist(val):
+    global tot_up_dist
+    tot_up_dist = val
+
+def compute_total_profile(nodes, active_nodes):
+    """
+    Computes the profile matrix of all active nodes.
+
+    Also sets the global variable for the total up distance. 
+
     Args:
-        seqs (str[]): matrix of dna sequences
+        nodes: all the nodes of the tree
+        active_nodes: the number of active nodes in the tree. 
 
     Returns:
         float[]: profile matrix of size 4 x k
     """
-    profile = make_empty_profile(len(seqs[0]))
-    for column in range(len(seqs[0])):
-        count = [0,0,0,0]
-        # go over every motif per column
-        for row in range(len(seqs)):
-            if seqs[row][column] == 'A':
-                count[0] += 1
-            elif seqs[row][column] == 'C':
-                count[1] += 1
-            elif seqs[row][column] == 'G':
-                count[2] += 1
-            else:
-                count[3] +=1
-        for profile_row in range(4):
-            profile[profile_row][column] = count[profile_row]/(len(seqs))
+    k = len(nodes[0].profile[0])
+    profile = make_empty_profile(k)
+    dist = 0
+    for node in nodes:
+        if node.is_active:
+            dist += node.up_distance
+            for row in range(4):
+                for col in range(k):
+                    profile[row][col] += node.profile[row][col]/active_nodes
+
+    set_total_up_dist(dist)
     return profile
 
 def find_total_profile(prof_i, nodes):
@@ -162,47 +188,82 @@ def find_total_profile(prof_i, nodes):
             total_profile += prof_dist(prof_i, node.profile)
     return total_profile
 
-def find_joins(nodes:list, active_nodes, seqs):
+def out_dist(node, total_profile, n):
+    """
+    Calculates the out distance from one node. 
+    """
+    return n*(prof_dist(node.profile, total_profile) + tot_up_dist - node.up_distance) / (n-2) 
+
+def calc_branch_len(i:Node, j:Node, n, total_profile) -> tuple[float, float]:
+    """
+    Calculates the branch length from daughter nodes i and j. 
+
+        Parameters:
+            i = node i 
+            j = node j
+            n = number of active nodes
+            total_profile = distance to all other nodes
+            tot_up_dist = total up distance
+    """
+    dist_i_j = node_dist(i, j)
+
+    r_i = out_dist(i, total_profile, n)
+    r_j = out_dist(j, total_profile, n)
+    res_i = (dist_i_j + r_i - r_j)/2
+    res_j = (dist_i_j + r_j - r_i)/2
+
+    return res_i, res_j
+
+def find_joins(nodes:list, seqs):
     """
     Loops through all active nodes and joins the nodes with the minimum distance. 
     Stops when the number of active nodes is just one. 
 
-    TODO Maybe removing the nodes from the list can be used instead of using a bool flag?
-    TODO Double check what equation to use for up_dist. In the supplementary they give a different equation?
     """
+    branch_lengths = []
+    active_nodes = len(nodes)
+    total_profile = compute_total_profile(nodes, active_nodes)
+    while active_nodes > 3:
+        i, j, mindist = find_min_dist_nodes(nodes, total_profile, active_nodes)
 
-    total_profile = compute_total_profile(seqs)
-    while active_nodes > 1:
-        a, b, mindist = find_min_dist_nodes(nodes, total_profile)
-        nodes[a].is_active = False 
-        nodes[b].is_active = False
-        prof_a = nodes[a].profile
-        prof_b = nodes[b].profile
-        c = profile_join(prof_a,prof_b)
-        # up_dist = (prof_dist(prof_a,c) + prof_dist(prof_b,c))/2 
-        up_dist = prof_dist(prof_a, prof_b)/2
-        new_node = Node(profile=c, up_distance=up_dist, is_active=True)
-        for seq in nodes[a].sequences:
-            new_node.add_sequence(seq)
-        for seq in nodes[b].sequences:
-            new_node.add_sequence(seq)
+        print(nodes[i].indexes, "<-->", nodes[j].indexes)
+        print("----------------")
+
+        prof_i = nodes[i].profile
+        prof_j = nodes[j].profile
+
+        nodes[i].is_active = False 
+        nodes[j].is_active = False
+
+        k = profile_join(prof_i, prof_j)
+        active_nodes-=1
+
+        up_dist = prof_dist(prof_i, prof_j)/2
+        new_node = Node(profile=k, up_distance=up_dist, is_active=True)
         nodes.append(new_node)
-        active_nodes -=1
-    
+
+        new_node.indexes = "("+ nodes[i].indexes + "," + nodes[j].indexes + ")"
+
+        total_profile = compute_total_profile(nodes, active_nodes)
+        i_len, j_len = calc_branch_len(nodes[i],nodes[j], active_nodes, total_profile)
+        branch_lengths.append(i_len)
+        branch_lengths.append(j_len)
+        # break
+    return(branch_lengths)
 
 def main():
-    seqs = read_file('./data/test-small.aln')
+    seqs = read_file('../data/test-small.aln')
+
     nodes = initialize_leaf_nodes(seqs)
-    active_nodes = len(nodes)
 
     ### PROOF that equation on the right bottom of page 3 in old paper equals comparison to 'total profile'. 
     # equation is needed for computing r(i) and r(j)
-    i = make_profile(seqs[0])
-    print(find_total_profile(i, nodes))
-    print(prof_dist(i, compute_total_profile(seqs))*len(seqs))
+    # i = make_profile(seqs[0])
+    # print(find_total_profile(i, nodes))
+    # print(prof_dist(i, compute_total_profile(seqs))*len(seqs))
     ###
     
-    find_joins(nodes, active_nodes, seqs)
-
+    branch_lengths = find_joins(nodes, seqs)
+    print(branch_lengths)
 if __name__ == '__main__':
     main()
--- src/node.py
+++ src/node.py
@@ -6,15 +6,14 @@ class Node:
     up_distance: float 
     is_active: bool
     sequences: list
+    indexes: str
 
     def __init__(self, profile=[], up_distance=0, is_active=False):
         self.profile = profile
         self.up_distance = up_distance
         self.is_active = is_active
         self.sequences = []
+        self.indexes = ""
     
     def set_active_status(self, val:bool):
         self.is_active = val
-
-    def add_sequence(self, seq:str):
-        self.sequences.append(seq)
