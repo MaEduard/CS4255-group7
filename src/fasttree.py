0a3990c05a37f6a552222b326f3fa69a0226cdba
MaEduard <eduard.ma6@gmail.com>
2023-01-19 16:48:30 +0100

    Work in progress

--- src/fasttree.py
+++ src/fasttree.py
@@ -1,7 +1,7 @@
+import random
 from sys import stdin
 import math
 from node import Node
-import random
 
 tot_up_dist = 0
 
@@ -24,21 +24,6 @@ def letter_to_index(let):
     elif let == "T":
         return 3
 
-
-def jukes_cantor(prof):
-
-    dist = -(3/4)*math.log(1-(4/3))
-
-def make_profile(seq):
-    """
-    Makes a new profile based on one sequence. 
-    """
-    prof = make_empty_profile(len(seq))
-    for i, let in enumerate(seq):
-        prof[letter_to_index(let)][i] = 1
-    return prof
-
-
 def make_empty_profile(size):
     """
     Creates an empty profile filled with zeros. 
@@ -50,16 +35,14 @@ def make_empty_profile(size):
 
     return profile
 
-
-def profile_join(a, j):
+def make_profile(seq):
     """
-    Joins two profile together and returns the result. 
+    Makes a new profile based on one sequence. 
     """
-    new_prof = make_empty_profile(len(a[0]))
-    for row in range(4):  # Only 4 nucleotides.
-        for col in range(len(a[0])):  # Assume same size for a and j
-            new_prof[row][col] = (a[row][col] + j[row][col])/2
-    return new_prof
+    prof = make_empty_profile(len(seq))
+    for i, let in enumerate(seq):
+        prof[letter_to_index(let)][i] = 1
+    return prof
 
 def node_dist(i:Node, j:Node):
     """
@@ -96,6 +79,23 @@ def r_equation(node_i, total_profile, n):
     ans = (n*prof_dist(node_i.profile, total_profile)) - prof_dist(node_i.profile, node_i.profile) - (n-1)*node_i.up_distance - (tot_up_dist - node_i.up_distance)
     return ans/(n-2)
 
+def profile_join(a, j):
+    """
+    Joins two profile together and returns the result. 
+    """
+    new_prof = make_empty_profile(len(a[0]))
+    for row in range(4):  # Only 4 nucleotides.
+        for col in range(len(a[0])):  # Assume same size for a and j
+            new_prof[row][col] = (a[row][col] + j[row][col])/2
+    return new_prof
+
+def out_dist(node, total_profile, n):
+    """
+    Calculates the out distance from one node. 
+    """
+    # return n*(prof_dist(node.profile, total_profile) + tot_up_dist - node.up_distance) / (n-2) 
+    return (n*(prof_dist(node.profile, total_profile)) - prof_dist(node.profile, node.profile) - tot_up_dist - (n-1)*node.up_distance) / (n-2) 
+
 def find_min_dist_nodes(nodes, total_profile, n):
     """
     Finds the minimum distance between all possible joins, and returns the index of both nodes. 
@@ -117,10 +117,7 @@ def find_min_dist_nodes(nodes, total_profile, n):
                     dist_i_j = node_dist(nodes[i], nodes[j])
                     r_i = out_dist(nodes[i], total_profile, n) # shouldn't we use r_equation?
                     r_j = out_dist(nodes[j], total_profile, n) # shouldn't we use r_equation?
-                    
                     dist_prime_i_j = dist_i_j - r_i - r_j
-
-                    print(dist_prime_i_j)
 
                     if dist_prime_i_j < min_dist:
                         min_dist = dist_prime_i_j
@@ -128,6 +125,72 @@ def find_min_dist_nodes(nodes, total_profile, n):
                         ind2 = j
 
     return ind1, ind2, min_dist
+
+def find_nodes_to_be_joined(nodes):
+    """
+    Finds the minimum distance between all possible joins, and returns the index of both nodes. 
+
+    Using exhaustive search. 
+
+    Parameters: 
+        n = number of active nodes
+
+    TODO include the r_equation to compute r(i) and r(j) to eventually compute d'(i,j) = d(i,j) - r(i) - r(j)
+    """
+    min_dist = float('inf')
+    node1 = None
+    node2 = None
+    ri = 0
+    rj = 0
+    for i in range(len(nodes)):
+        if nodes[i].is_active: # Skip nodes that are inactive. 
+            current_dist = nodes[i].top_hits[0][0] 
+            if current_dist < min_dist:
+                min_dist = current_dist
+                node1 = nodes[i]
+                node2 = nodes[i].top_hits[0][2]
+
+    return node1, node2 
+
+def merge_top_hits(top_hits1, top_hits2):
+    top_hits_merged = top_hits1 + top_hits2
+    top_hits_merged = list(set(top_hits_merged))
+
+    return top_hits_merged
+
+def filter_top_hits(merged_node, top_hits, active_nodes):
+    new_top_hits = []
+    for node_tuple in top_hits:
+        node = node_tuple[2]
+        dist_i_j = node_dist(merged_node, node)
+        r_j = out_dist(merged_node, total_profile, active_nodes) 
+        r_i = out_dist(node, total_profile, active_nodes) 
+        dist_prime_i_j = dist_i_j - r_i - r_j 
+        new_top_hits.append((dist_prime_i_j, node.index, node))
+    
+    new_top_hits.sort()
+    return new_top_hits[0:top_hits_length]
+
+
+def join_nodes(node1, node2, active_nodes):
+    # create new profile
+    # create new top hits list
+    # set name of new node
+    # create new up_distance
+    # update total_profile?
+    # set node1 and node2 to inactive
+
+    new_profile = profile_join(node1.profile, node2.profile)
+    new_updistance = prof_dist(node1.profile, node2.profile)
+    merged_node = Node(profile=new_profile, up_distance=new_updistance, is_active=True)
+    top_hits_merged = merge_top_hits(node1.top_hits, node2.top_hits)
+    filtered_top_hits = filter_top_hits(merged_node, top_hits_merged, active_nodes)
+    merged_node.top_hits = filtered_top_hits
+
+    node1.is_active = False
+    node2.is_active = False
+
+    return merged_node
 
 def initialize_leaf_nodes(seqs) -> list:
     """ 
@@ -137,13 +200,11 @@ def initialize_leaf_nodes(seqs) -> list:
     for index, seq in enumerate(seqs):
         profile = make_profile(seq)
         node = Node(profile=profile, up_distance=0, is_active=True)
-        node.indexes = str(index)
+        node.value = str(index)
+        node.index = index
+        node.main_sequence = seq
         nodes.append(node)    
     return nodes
-
-def print_profile(a):
-    for row in a:
-        print(row)
 
 def set_total_up_dist(val):
     global tot_up_dist
@@ -193,13 +254,6 @@ def find_total_profile(prof_i, nodes):
             total_profile += prof_dist(prof_i, node.profile)
     return total_profile
 
-def out_dist(node, total_profile, n):
-    """
-    Calculates the out distance from one node. 
-    """
-    # return n*(prof_dist(node.profile, total_profile) + tot_up_dist - node.up_distance) / (n-2) 
-    return (n*(prof_dist(node.profile, total_profile)) - prof_dist(node.profile, node.profile) - tot_up_dist - (n-1)*node.up_distance) / (n-2) 
-
 def calc_branch_len(i:Node, j:Node, n, total_profile) -> tuple[float, float]:
     """
     Calculates the branch length from daughter nodes i and j. 
@@ -225,99 +279,96 @@ def find_joins(nodes:list, seqs):
     Loops through all active nodes and joins the nodes with the minimum distance. 
     Stops when the number of active nodes is just two. 
     """
-
     branch_lengths = []
     active_nodes = len(nodes)
     global total_profile
+    global top_hits_length
+    global node_counter
+    node_counter = active_nodes-1
+    top_hits_length = int(math.sqrt(len(nodes)))
     total_profile = compute_total_profile(nodes, active_nodes)
-
-    seed_nodes = [node for node in nodes]
-    # create_top_hits(seed_nodes, nodes, len(nodes))
+    create_top_hits(nodes, len(nodes), total_profile)
     while active_nodes > 2:
         total_profile = compute_total_profile(nodes, active_nodes)
-        i, j, mindist = find_min_dist_nodes(nodes, total_profile, active_nodes)
-        # print(mindist)
+        node1, node2 = find_nodes_to_be_joined(nodes)
+        node12 = join_nodes(node1, node2, active_nodes)
+        # i, j, mindist = find_min_dist_nodes(nodes, total_profile, active_nodes)
 
-        print(nodes[i].indexes, "<-->", nodes[j].indexes)
+        print(node1.value, "<-->", node2.value)
         print("----------------")
 
-        prof_i = nodes[i].profile
-        prof_j = nodes[j].profile
+        # prof_i = nodes[i].profile
+        # prof_j = nodes[j].profile
 
-        nodes[i].is_active = False 
-        nodes[j].is_active = False
+        # nodes[i].is_active = False 
+        # nodes[j].is_active = False
 
-        k = profile_join(prof_i, prof_j)
+        # k = profile_join(prof_i, prof_j)
 
-        up_dist = prof_dist(nodes[i].profile, nodes[j].profile)/2 # node_dist(nodes[i], nodes[j])/2 
-        new_node = Node(profile=k, up_distance=up_dist, is_active=True)
-        nodes.append(new_node)
+        # up_dist = prof_dist(nodes[i].profile, nodes[j].profile)/2 # node_dist(nodes[i], nodes[j])/2 
+        # new_node = Node(profile=k, up_distance=up_dist, is_active=True)
+        nodes.append(node12)
 
-        new_node.indexes = "("+ nodes[i].indexes + "," + nodes[j].indexes + ")"
+        node12.value = "("+ node1.value + "," + node2.value + ")"
 
-        
-        i_len, j_len = calc_branch_len(nodes[i],nodes[j], active_nodes, total_profile)
-        branch_lengths.append(i_len)
-        branch_lengths.append(j_len)
+        ## REDO THIS? 
+        # i_len, j_len = calc_branch_len(nodes[i],nodes[j], active_nodes, total_profile)
+        # branch_lengths.append(i_len)
+        # branch_lengths.append(j_len)
         active_nodes-=1
         # break
-    
     return(branch_lengths)
 
-# def create_top_hits(seed_nodes, nodes, n):
-#     m = int(math.sqrt(len(nodes)))
+def create_top_hits(nodes, n, total_profile):
+    m = int(math.sqrt(len(nodes))) # maybe use math.ceil? 
+    unused_nodes = [i for i in range(len(nodes))]
 
-#     while(len(seed_nodes) > 0):
-#         seed_idx = random.randint(0, len(seed_nodes)-1) #pick random seed node. Note that randint picks random int from inclusive range, hence do len(seed_nodes) - 1
-#         seed = seed_nodes[seed_idx]
-#         top_hits = []
-#         r_seed = out_dist(seed, total_profile, n)
+    while(len(unused_nodes) > 0):
+        seed_idx = random.sample(unused_nodes, 1)[0] #pick random seed node. Note that randint picks random int from inclusive range, hence do len(seed_nodes) - 1
+        seed = nodes[seed_idx]
+        top_hits = []
+        r_seed = out_dist(seed, total_profile, n)
 
-#         for node in nodes:
-#             if node != seed:
-#                 dist_i_j = node_dist(node, seed)
-#                 r_j = out_dist(node, total_profile, n) # shouldn't we use r_equation?
-#                 dist_prime_i_j = dist_i_j - r_seed - r_j
-#                 top_hits.append((dist_prime_i_j, node))
+        for node in nodes:
+            if node.main_sequence != seed.main_sequence:
+                dist_i_j = node_dist(node, seed)
+                r_j = out_dist(node, total_profile, n) # shouldn't we use r_equation?
+                dist_prime_i_j = dist_i_j - r_seed - r_j
+                top_hits.append((dist_prime_i_j, node.index, node))
 
-#         top_hits.sort()
+        top_hits.sort() # the distances for some top hits are apparently exactly the same and thus it starts to compare nodes, which is impossible. 
 
-#         nodes[seed_idx].top_hits = top_hits[0:m]
-#         seed_nodes.remove(seed)
+        nodes[seed_idx].top_hits = top_hits[0:m]
+        unused_nodes.remove(seed_idx)
 
-#         print(seed_nodes)
-
-#         for neighbor in top_hits[0:m]:
-#             neighbor_node = neighbor[1]
-#             top_hits_neighbor = []
-#             for node_tuple in top_hits[0:2*m]:
-#                 node = node_tuple[1]
-#                 if node != neighbor and len(node.top_hits) == 0:
-#                     dist_i_j = node_dist(neighbor_node, node)
-#                     r_j = out_dist(neighbor_node, total_profile, n) 
-#                     r_i = out_dist(node, total_profile, n)
-#                     dist_prime_i_j = dist_i_j - r_i - r_j
-#                     top_hits_neighbor.append((dist_prime_i_j, node))
-#             top_hits_neighbor.sort()
-#             print("Top hits length:")
-#             print(len(top_hits))
-#             neighbor_node.top_hits = top_hits_neighbor
-#             print(neighbor_node)
-#             seed_nodes.remove(neighbor_node)
-
+        for neighbor in top_hits[0:m]:
+            neighbor_node = neighbor[2]
+            if len(neighbor_node.top_hits) != 0:
+                continue
+            top_hits_neighbor = []
+            for node_tuple in top_hits[0:2*m]:
+                node = node_tuple[2]
+                if node.main_sequence != neighbor_node.main_sequence:
+                    dist_i_j = node_dist(neighbor_node, node) 
+                    r_j = out_dist(neighbor_node, total_profile, n) 
+                    r_i = out_dist(node, total_profile, n) 
+                    dist_prime_i_j = dist_i_j - r_i - r_j 
+                    top_hits_neighbor.append((dist_prime_i_j, node.value, node)) # add node.value to make sure that if two nodes have exactly the same distance_prime_i_j, pick one randomly
+                    
+            top_hits_neighbor.sort()
+            neighbor_node.top_hits = top_hits_neighbor[0:m]
+            if neighbor_node in unused_nodes:
+                unused_nodes.remove(neighbor_node)
 
 def main():
     seqs = read_file('data/test-small.aln')
-    print(seqs)
+
+
+    ### TODO ###
+    # perhaps change the top_hits list to contain the index of the node instad of the node object itself.
+
     nodes = initialize_leaf_nodes(seqs)
 
-    ### PROOF that equation on the right bottom of page 3 in old paper equals comparison to 'total profile'. 
-    # equation is needed for computing r(i) and r(j)
-    # i = make_profile(seqs[0])
-    # print(find_total_profile(i, nodes))
-    # print(prof_dist(i, compute_total_profile(seqs))*len(seqs))
-    ###
-    
     branch_lengths = find_joins(nodes, seqs)
     print(branch_lengths)
 
--- src/node.py
+++ src/node.py
@@ -8,6 +8,8 @@ class Node:
     sequences: list
     top_hits = list
     indexes: str
+    index: int
+    value: str #name of the node itself --> e.g. node 1 merged with node 2 is called '12'
     main_sequence = ""
 
     def __init__(self, profile=[], up_distance=0, is_active=False, main_sequence="", top_hits = []):
@@ -17,6 +19,8 @@ class Node:
         self.top_hits = []
         self.indexes = None
         self.main_sequence = ""
-    
+        self.index = -1
+        self.value = ""
+
     def set_active_status(self, val:bool):
         self.is_active = val
